216db207a8de11ab2d04a7a7b7564b9ed4838fcb
cawauchi6204 <cawauchi6204@gmail.com>
2020-06-08 11:56:31 +0900

    first commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBDcrFz8STQvnxZ1sQ9nCYwEG9DJAYxtls",
+    authDomain: "ptit-a-ptit.firebaseapp.com",
+    databaseURL: "https://ptit-a-ptit.firebaseio.com",
+    projectId: "ptit-a-ptit",
+    storageBucket: "ptit-a-ptit.appspot.com",
+    messagingSenderId: "1057050321505",
+    appId: "1:1057050321505:web:dc20f60c880cbad31d8020",
+    measurementId: "G-E2VXCW9KWL",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
